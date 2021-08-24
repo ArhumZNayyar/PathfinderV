6e64f6964786847c4e45936e4865e75cab90666d
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2021-08-24 01:10:55 -0400

    Updated screen dimensions

--- Global.h
+++ Global.h
@@ -4,8 +4,8 @@ namespace globals {
 	/* Application Info */
 	const int FPS = 60;
 	const int maxFrameTime = 1000 / FPS;
-	const int screenWidth = 815;
-	const int screenHeight = 715;
+	const int screenWidth = 870;
+	const int screenHeight = 620;
 
 	/* Map Info */
 	// Map dimensions (Note: It is less than the screen width/height) so text can be displayed outside the map
